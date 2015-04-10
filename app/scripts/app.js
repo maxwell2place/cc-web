52d1a1966217c33d981199f7e80645c6dcef6a94
zzsanduo <zzsanduo@gmail.com>
2015-04-10 21:25:40 +0800

    init the cc-web with scoffold

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webappApp
+ * @description
+ * # webappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webappApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'smart-table',
+    'ui.bootstrap'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main-controller.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webappApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webappApp
+ */
+angular.module('webappApp')
+  .controller('MainController', function ($scope) {
+        $scope.count = 3;
+  });
